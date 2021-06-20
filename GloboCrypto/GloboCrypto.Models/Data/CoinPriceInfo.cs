366691818e96cd955ac618edae482f13476e714e
Steve Kay <stevekay72@gmail.com>
2021-06-20 19:45:32 +0100

    Up to Slide 7

---
+++ GloboCrypto/GloboCrypto.Models/Data/CoinPriceInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboCrypto.Models.Data
+{
+    public class CoinPriceInfo
+    {
+        public string Id { get; set; }
+        public string Price { get; set; }
+        public string PriceTimestamp { get; set; }
+        public string Intervak { get; set; }
+        public string PriceChange { get; set; }
+        public string PriceChangePct { get; set; }
+
+    }
+}
