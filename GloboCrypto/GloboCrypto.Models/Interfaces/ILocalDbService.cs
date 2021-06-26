0302fd0d896fada9ba96377bbc5a6181b1cbae36
Steve Kay <stevekay72@gmail.com>
2021-06-26 07:50:00 +0100

    xx

--- GloboCrypto/GloboCrypto.Models/Interfaces/ILocalDbService.cs
+++ GloboCrypto/GloboCrypto.Models/Interfaces/ILocalDbService.cs
@@ -1,7 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
 namespace GloboCrypto.Models.Interfaces
 {
     public interface ILocalDbService
     {
+        void Delete<T>(Expression<Func<T, bool>> query);
+        void Insert<T>(T item);
+        void Upsert<T>(T item);
+        IEnumerable<T> All<T>();
+        IEnumerable<T> Query<T>(Expression<Func<T, bool>> query);
     }
 }
---
+++ GloboCrypto/GloboCrypto.WebAPI.Services/Data/LocalDbService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using GloboCrypto.Models.Interfaces;
+using LiteDB;
+
+namespace GloboCrypto.WebAPI.Services.Data
+{
+    public class LocalDbService : ILocalDbService
+    {
+        private readonly LiteDatabase Local;
+
+        private string GetFilePath(string fileName) => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), fileName);
+
+        public LocalDbService(string fileName)
+        {
+            Local = new LiteDatabase($"filename={GetFilePath(fileName)};mode=Exclusive");
+        }
+
+        ~LocalDbService()
+        {
+            Local.Dispose();
+        }
+
+        public IEnumerable<T> All<T>()
+        {
+            var collection = Local.GetCollection<T>();
+            return collection.FindAll().Skip(0).Take(100);
+        }
+
+        public void Insert<T>(T item)
+        {
+            var collection = Local.GetCollection<T>();
+            collection.Insert(item);
+        }
+
+        public void Upsert<T>(T item)
+        {
+            var collection = Local.GetCollection<T>();
+            collection.Upsert(item);
+        }
+
+        public void Delete<T>(Expression<Func<T, bool>> query)
+        {
+            var collection = Local.GetCollection<T>();
+            collection.DeleteMany(query);
+        }
+
+        public IEnumerable<T> Query<T>(Expression<Func<T, bool>> query)
+        {
+            var collection = Local.GetCollection<T>();
+            return collection.Find(query);
+        }
+    }
+}
