46bca520d1cc701b8e0a8f5415dc4c5c48fea2c9
Steve Kay <stevekay72@gmail.com>
2021-07-03 07:50:32 +0100

    zxzxzxz

---
+++ GloboCrypto/GloboCrypto.WebAPI.Services/Notifications/INotificationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using GloboCrypto.Models.Notifications;
+
+namespace GloboCrypto.WebAPI.Services.Notifications
+{
+    public interface INotificationService
+    {
+        Task CheckAndNotifyAsync();
+        Task<NotificationSubscription> SubscribeAsync(string userId, NotificationSubscription subscription);
+        Task UpdateSubscription(string userId, string coinIds);
+    }
+}
---
+++ GloboCrypto/GloboCrypto.WebAPI.Services/Notifications/NotificationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using GloboCrypto.Models.Notifications;
+using GloboCrypto.WebAPI.Services.Data;
+
+namespace GloboCrypto.WebAPI.Services.Notifications
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly ILocalDbService LocalDb;
+
+        public NotificationService(ILocalDbService localDb)
+        {
+            LocalDb = localDb;
+        }
+
+        public Task CheckAndNotifyAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NotificationSubscription> SubscribeAsync(string userId, NotificationSubscription subscription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateSubscription(string userId, string coinIds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
