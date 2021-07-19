e06f2da97afe5dc77c8ae140b51523f397da70af
Steve Kay <stevekay72@gmail.com>
2021-07-19 19:15:43 +0100

    end of M4

--- GloboCrypto/GloboCrypto.PWA/wwwroot/service-worker.published.js
+++ GloboCrypto/GloboCrypto.PWA/wwwroot/service-worker.published.js
@@ -11,6 +11,17 @@ const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
 const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 
+const notifyNewVersion = () => {
+    const bc = new BroadcastChannel('blazor-channel');
+    bc.postMessage('new-version-found');
+    bc.onmessage = function (message) {
+        if (message && message.data == "skip-waiting") {
+            self.skipWaiting();
+            bc.postMessage("reload-page");
+        }
+    }
+}
+
 async function onInstall(event) {
     console.info('Service worker: Install');
 
@@ -20,6 +31,7 @@ async function onInstall(event) {
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
         .map(asset => new Request(asset.url, { integrity: asset.hash }));
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+    notifyNewVersion();
 }
 
 async function onActivate(event) {
